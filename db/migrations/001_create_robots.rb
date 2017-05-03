322e1fc4494666a8e991351a29f36991e4661ffe
Rhonda <rwilhel@gmail.com>
2017-05-02 22:05:28 -0600

    Progress made on Robot World app

---
+++ db/migrations/001_create_robots.rb
@@ -0,0 +1,13 @@
+# db/migrations/001_create_robots.rb
+
+require 'sqlite3'
+
+database = SQLite3::Database.new("db/robot_world_development.db")
+database.execute("CREATE TABLE robots (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                       name VARCHAR(64),
+                                       city VARCHAR(64),
+                                       state VARCHAR(64),
+                                       dept VARCHAR(64)
+                                     );"
+                )
+puts "creating robots table for development"
